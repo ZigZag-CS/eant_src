bb3df64dd99925d239044b6d9cf678df91ee8dbe
zigZag <ps96068@yahoo.com>
2020-06-13 16:49:05 +0300

    7. Cart Component == 66. Cart Model Manager Part 2

--- apps/carts/models.py
+++ apps/carts/models.py
@@ -5,12 +5,45 @@ from apps.products.models import *
 
 User = settings.AUTH_USER_MODEL
 
+
+class CartManager(models.Manager):
+
+    # def get_or_create(self):
+    #     return obj, True
+
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            # print("Cart ID exists")
+            cart_obj = qs.first()
+            if request.user.is_authenticated and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = self.new(user=request.user)
+            new_obj = True
+            request.session["cart_id"] = cart_obj.id
+        return cart_obj, new_obj
+
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated:
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
+
+
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
