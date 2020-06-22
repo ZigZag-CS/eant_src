e63bfaa3cfc79ddad896f913901a14eb49d1feb8
ZigZag-CS <Qaser45%23nbr>
2020-06-22 16:55:51 +0300

    10. Products & Async == 110. Finalize Cart Updating with Ajax

--- apps/products/views.py
+++ apps/products/views.py
@@ -32,9 +32,24 @@ class ProductListView(ListView):
     #     print(f'Context: {context}')
     #     return context
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
     def get_queryset(self, *args, **kwargs):
         request = self.request
         return Product.objects.all()
+
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/list.html", context)
+
 
 
 class ProductDetailSlugView(DetailView):
@@ -86,3 +101,32 @@ class ProductDetailView(DetailView):
     #     request = self.request
     #     pk = self.kwargs.get('pk')
     #     return Product.objects.filter(pk=pk)
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    # instance = Product.objects.get(pk=pk, featured=True) #id
+    # instance = get_object_or_404(Product, pk=pk, featured=True)
+    # try:
+    #     instance = Product.objects.get(id=pk)
+    # except Product.DoesNotExist:
+    #     print('no product here')
+    #     raise Http404("Product doesn't exist")
+    # except:
+    #     print("huh?")
+
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404("Product doesn't exist")
+    #print(instance)
+    # qs  = Product.objects.filter(id=pk)
+
+    # #print(qs)
+    # if qs.exists() and qs.count() == 1: # len(qs)
+    #     instance = qs.first()
+    # else:
+    #     raise Http404("Product doesn't exist")
+
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
