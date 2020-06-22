8ff4d583c9b8b3e376e4e94d7979256616ece664
ZigZag-CS <Qaser45%23nbr>
2020-06-22 12:27:14 +0300

    10. Products & Async == 105. Handle Ajax in Django with JsonResponse

--- apps/carts/views.py
+++ apps/carts/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from apps.accounts.forms import *
@@ -18,8 +19,7 @@ def cart_home(request):
 def cart_update(request):
     # print(request.POST)
     product_id = request.POST.get("product_id")
-    if request.is_ajax():
-        print("Ajax request")
+
     if product_id is not None:
         try:
             product_obj = Product.objects.get(id=product_id)
@@ -29,10 +29,19 @@ def cart_update(request):
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
+            added = False
         else:
             cart_obj.products.add(product_obj) #cart_obj.products.add(product_obj)
+            added = True
         request.session['cart_items'] = cart_obj.products.count()
         # return redirect(product_obj.get_absolute_url())
+        if request.is_ajax():
+            print("Ajax request")
+            json_data = {
+                "added": added,
+                "removed": not added,
+            }
+            return JsonResponse(json_data)
     return redirect("carts:home")
 
 
