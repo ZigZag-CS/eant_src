d3b85a7b3e1d13cf4c98624215185816e293d2e1
ZigZag-CS <Qaser45%23nbr>
2020-07-02 11:39:15 +0300

    16. Account & Settings +++ Final Addresses on Order - cart model adaugat 2 campuri, shablonul s-a modificat la order si carts

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -54,19 +54,27 @@ class OrderManager(models.Manager):
 
 # Random, Unique
 class Order(models.Model):
-    billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null=True, blank=True)
-    order_id        = models.CharField(max_length=120, blank=True) # AB31DE3
-    shipping_address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name="shipping_address", null=True, blank=True)
-    billing_address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name="billing_address", null=True, blank=True)
-    cart            = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    status          = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
-    shipping_total  = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
-    total           = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
-    active          = models.BooleanField(default=True)
-    updated = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    billing_profile     = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null=True, blank=True)
+    order_id            = models.CharField(max_length=120, blank=True) # AB31DE3
+    shipping_address    = models.ForeignKey(Address, on_delete=models.CASCADE, related_name="shipping_address", null=True, blank=True)
+    billing_address     = models.ForeignKey(Address, on_delete=models.CASCADE, related_name="billing_address", null=True, blank=True)
+    shipping_address_final = models.TextField(blank=True, null=True)
+    billing_address_final = models.TextField(blank=True, null=True)
+    cart                = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    status              = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
+    shipping_total      = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
+    total               = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    active              = models.BooleanField(default=True)
+    updated             = models.DateTimeField(auto_now=True)
+    timestamp           = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.order_id
 
     objects = OrderManager()
+
+    class Meta:
+        ordering = ['-timestamp', '-updated']
 
     def get_absolute_url(self):
         return reverse("orders:detail", kwargs={"order_id": self.order_id})
@@ -102,21 +110,20 @@ class Order(models.Model):
         return self.status
 
 
-    def __str__(self):
-        return self.order_id
-
-    class Meta:
-        ordering = ['-timestamp', '-updated']
-
-
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
-    print("in => pre_save_create_order_id")
+    # print("in => pre_save_create_order_id")
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
     qs = Order.objects.filter(cart=instance.cart).exclude(billing_profile=instance.billing_profile)
     if qs.exists():
         qs.update(active=False)
+
+    if instance.shipping_address and not instance.shipping_address_final:
+        instance.shipping_address_final = instance.shipping_address.get_address()
+
+    if instance.billing_address and not instance.billing_address_final:
+        instance.billing_address_final = instance.billing_address.get_address()
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
 
